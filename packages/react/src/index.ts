4807bc49253f37a8c168bff871a06e8f66d65e45
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2022-07-06 13:42:30 -0300

    feat: make useSuperState rerender with checksum

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-import { IDraft, ISuperState } from '@superstate/core'
+import { ISuperState } from '@superstate/core'
 
 /**
  * A React hook that re-renders the component when a superstate changes.
@@ -14,18 +14,32 @@ export function useSuperState<S>(
   ss: Pick<ISuperState<S>, 'subscribe' | 'now' | 'draft'>,
   options?: IUseSuperStateOptions
 ) {
-  const [, setNow] = useState<S>(ss.now())
-  const [, setDraft] = useState<IDraft<S>>(ss.draft())
+  const [, setNow] = useState(_getRandomDateForChecksum())
+  const [, setDraft] = useState(_getRandomDateForChecksum())
+
+  function _rerender(target?: ISuperstateTarget) {
+    switch (target) {
+      case 'now':
+        setNow(_getRandomDateForChecksum())
+        break
+      case 'draft':
+        setDraft(_getRandomDateForChecksum())
+        break
+      default:
+        setNow(_getRandomDateForChecksum())
+        setDraft(_getRandomDateForChecksum())
+    }
+  }
 
   useEffect((): (() => void) => {
     const unsubs: (() => void)[] = []
 
     if (!options?.target || options.target === 'now') {
-      unsubs.push(ss.subscribe(setNow))
+      unsubs.push(ss.subscribe(() => _rerender('now')))
     }
 
     if (!options?.target || options.target === 'draft') {
-      unsubs.push(ss.subscribe(setDraft, 'draft'))
+      unsubs.push(ss.subscribe(() => _rerender('draft')))
     }
 
     return () => unsubs.forEach((u) => u())
@@ -38,5 +52,11 @@ interface IUseSuperStateOptions {
   /**
    * The target to monitor. If not specified, whenever either `now` or `draft` changes, the component will re-render.
    */
-  target: 'now' | 'draft'
+  target: ISuperstateTarget
+}
+
+type ISuperstateTarget = 'now' | 'draft'
+
+function _getRandomDateForChecksum() {
+  return new Date(Math.floor(Math.random() * 10000000000))
 }
